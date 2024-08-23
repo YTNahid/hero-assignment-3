1eee73da4fa1900f2aabc9a1b91a878d3a304cd0
YTNahid <nahid1coc@gmail.com>
2024-08-23 23:15:14 +0600

    Made Mobile Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
                 'text-color-1': '#494949',
                 'text-color-2': '#737373',
                 'accent-color': '#ABEF5F',
+                'border-color': '#e9e9e9',
+                'bg-color': '#f4f4f4',
+            },
+            fontFamily: {
+                manrope: ['Manrope', 'sans-serif'],
             },
         },
     },
