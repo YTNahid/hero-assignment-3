c0e63910e2e5819d194740b08c3e4bb9d7829d49
YTNahid <nahid1coc@gmail.com>
2024-08-24 01:08:23 +0600

    Made Hero Section for Mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./*.{html,js}'],
+    content: ['./*.html'],
     theme: {
         extend: {
             colors: {
                 'primary-color': '#212121',
-                'text-color-1': '#494949',
-                'text-color-2': '#737373',
+                'text-color-dark': '#494949',
+                'text-color-light': '#737373',
                 'accent-color': '#ABEF5F',
                 'border-color': '#e9e9e9',
                 'bg-color': '#f4f4f4',
